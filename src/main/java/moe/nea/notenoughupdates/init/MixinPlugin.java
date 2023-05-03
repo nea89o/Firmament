ce3b6681c95a45ef5ec08160e0d4529e4685ebfa
nea <nea@nea.moe>
2023-05-03 20:02:22 +0200

    Remove some hypixel warnings

---
+++ src/main/java/moe/nea/notenoughupdates/init/MixinPlugin.java
@@ -0,0 +1,47 @@
+package moe.nea.notenoughupdates.init;
+
+import moe.nea.notenoughupdates.NotEnoughUpdates;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return Boolean.getBoolean("notenoughupdates.debug") ||
+            !mixinClassName.startsWith("moe.nea.notenoughupdates.mixins.devenv");
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
---
+++ src/main/java/moe/nea/notenoughupdates/mixins/devenv/DisableInvalidFishingHook.java
@@ -0,0 +1,15 @@
+package moe.nea.notenoughupdates.mixins.devenv;
+
+import net.minecraft.entity.projectile.FishingBobberEntity;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(FishingBobberEntity.class)
+public class DisableInvalidFishingHook {
+    @Redirect(method = "onSpawnPacket", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"))
+    public void onOnSpawnPacket(Logger instance, String s, Object o, Object o1) {
+        // Don't warn for broken fishing hooks, since HyPixel sends a bunch of those
+    }
+}
