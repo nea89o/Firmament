b65382a7b74836e13d8d9c9104971737288d1496
nea <nea@nea.moe>
2023-06-05 12:38:20 +0200

    Add more decimals to pet numbers

--- src/main/kotlin/moe/nea/firmament/rei/SBItemEntryDefinition.kt
+++ src/main/kotlin/moe/nea/firmament/rei/SBItemEntryDefinition.kt
@@ -79,10 +79,10 @@ data class SBItemStack(
                 ?.interpolatedStatsAtLevel(petData.levelData.currentLevel)
             if (stats != null) {
                 stats.otherNumbers.forEachIndexed { index, it ->
-                    replacementData[index.toString()] = FirmFormatters.toString(it, 0)
+                    replacementData[index.toString()] = FirmFormatters.toString(it, 1)
                 }
                 stats.statNumbers.forEach { (t, u) ->
-                    replacementData[t] = FirmFormatters.toString(u, 0)
+                    replacementData[t] = FirmFormatters.toString(u, 1)
                 }
             }
             replacementData["LVL"] = petData.levelData.currentLevel.toString()
