98b778d0dd123fa1e68a4e9196ed00acfe123ac7
nea <nea@nea.moe>
2023-06-10 22:37:58 +0200

    Add world keyboard event

---
+++ src/main/java/moe/nea/firmament/mixins/MixinKeyboard.java
@@ -0,0 +1,17 @@
+package moe.nea.firmament.mixins;
+
+import moe.nea.firmament.events.WorldKeyboardEvent;
+import net.minecraft.client.Keyboard;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Keyboard.class)
+public class MixinKeyboard {
+
+    @Inject(method = "onKey", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;onKeyPressed(Lnet/minecraft/client/util/InputUtil$Key;)V"))
+    public void onKeyBoardInWorld(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
+        WorldKeyboardEvent.Companion.publish(new WorldKeyboardEvent(key, scancode, modifiers));
+    }
+}
---
+++ src/main/kotlin/moe/nea/firmament/events/WorldKeyboardEvent.kt
@@ -0,0 +1,16 @@
+package moe.nea.firmament.events
+
+import net.minecraft.client.option.KeyBinding
+import moe.nea.firmament.keybindings.IKeyBinding
+
+data class WorldKeyboardEvent(val keyCode: Int, val scanCode: Int, val modifiers: Int) : FirmamentEvent.Cancellable() {
+    companion object : FirmamentEventBus<WorldKeyboardEvent>()
+
+    fun matches(keyBinding: KeyBinding): Boolean {
+        return matches(IKeyBinding.minecraft(keyBinding))
+    }
+
+    fun matches(keyBinding: IKeyBinding): Boolean {
+        return keyBinding.matches(keyCode, scanCode, modifiers)
+    }
+}
