2042d63e3f50d364b29e8c59a517a6ca500fc0cb
Linnea Gräf <nea@nea.moe>
2024-05-06 17:58:01 +0200

    Improve shell.nix
    [no changelog]

--- shell.nix
+++ shell.nix
@@ -13,10 +13,30 @@ pkgs.mkShell {
     glfw
     jdk21
     libGL
+    wayland
+    flite
+    jack2
+    openal
+    pulseaudio
+    pipewire
+    glibc
   ];
   shellHook = ''
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.glfw}/lib"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.pipewire}/lib"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.pulseaudio}/lib"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.jack2}/lib"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.openal}/lib"
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.libGL}/lib"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.wayland}/lib"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.flite}/lib"
     export JAVA_HOME=${pkgs.jdk21}
+    apply() {
+        echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >.env
+        echo org.lwjgl.glfw.libname=${pkgs.glfw}/lib/libglfw.so >.properties
+        echo "jna.library.path=$LD_LIBRARY_PATH" >>.properties
+        echo "java.library.path=$LD_LIBRARY_PATH" >>.properties
+    }
+
   '';
 }
