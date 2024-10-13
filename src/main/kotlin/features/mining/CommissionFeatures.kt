a40c50b99186d425b3fcf42336940c29b97a8219
Linnea Gräf <nea@nea.moe>
2024-10-13 22:00:04 +0200

    Add completed commission highlight

---
+++ src/main/kotlin/features/mining/CommissionFeatures.kt
@@ -0,0 +1,29 @@
+package moe.nea.firmament.features.mining
+
+import net.minecraft.util.Identifier
+import moe.nea.firmament.annotations.Subscribe
+import moe.nea.firmament.events.SlotRenderEvents
+import moe.nea.firmament.gui.config.ManagedConfig
+import moe.nea.firmament.util.MC
+import moe.nea.firmament.util.mc.loreAccordingToNbt
+import moe.nea.firmament.util.unformattedString
+
+object CommissionFeatures {
+	object Config : ManagedConfig("commissions", Category.MINING) {
+		val highlightCompletedCommissions by toggle("highlight-completed") { true }
+	}
+
+
+	@Subscribe
+	fun onSlotRender(event: SlotRenderEvents.Before) {
+		if (!Config.highlightCompletedCommissions) return
+		if (MC.screenName != "Commissions") return
+		val stack = event.slot.stack
+		if(stack.loreAccordingToNbt.any { it.unformattedString == "COMPLETED" }) {
+			event.context.drawSprite(
+				event.slot.x, event.slot.y, 0, 16, 16,
+				MC.guiAtlasManager.getSprite(Identifier.of("firmament:completed_commission_background"))
+			)
+		}
+	}
+}
