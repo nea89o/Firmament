9df1f12970dafdbf4cb13c76c1df1caed66f4a59
Linnea Gräf <nea@nea.moe>
2024-11-27 15:57:02 +0100

    fix: Incorrect primary codec in lenient profile serialization

--- src/main/java/moe/nea/firmament/mixins/LenientProfileComponentPatch.java
+++ src/main/java/moe/nea/firmament/mixins/LenientProfileComponentPatch.java
@@ -3,6 +3,10 @@ package moe.nea.firmament.mixins;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.Lifecycle;
+import com.mojang.util.UndashedUuid;
+import moe.nea.firmament.util.json.FirmCodecs;
 import net.minecraft.component.type.ProfileComponent;
 import net.minecraft.util.Uuids;
 import org.objectweb.asm.Opcodes;
@@ -13,9 +17,9 @@ import java.util.UUID;
 
 @Mixin(ProfileComponent.class)
 public class LenientProfileComponentPatch {
-    // lambda in RecordCodecBuilder.create for BASE_CODEC
-    @ModifyExpressionValue(method = "method_57508", at = @At(value = "FIELD", opcode = Opcodes.GETSTATIC, target = "Lnet/minecraft/util/Uuids;INT_STREAM_CODEC:Lcom/mojang/serialization/Codec;"))
-    private static Codec<UUID> onStaticInit(Codec<UUID> original) {
-        return Uuids.CODEC;
-    }
+	// lambda in RecordCodecBuilder.create for BASE_CODEC
+	@ModifyExpressionValue(method = "method_57508", at = @At(value = "FIELD", opcode = Opcodes.GETSTATIC, target = "Lnet/minecraft/util/Uuids;INT_STREAM_CODEC:Lcom/mojang/serialization/Codec;"))
+	private static Codec<UUID> onStaticInit(Codec<UUID> original) {
+		return FirmCodecs.UUID_LENIENT_PREFER_INT_STREAM;
+	}
 }
