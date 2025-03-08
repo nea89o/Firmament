cf63211a9f953d2be7d12b8b748e9048710e21dc
Linnea Gräf <nea@nea.moe>
2025-03-08 14:22:25 +0100

    chore: Clean up some TODOs

--- src/compat/jade/java/moe/nea/firmament/compat/jade/FirmamentJadePlugin.kt
+++ src/compat/jade/java/moe/nea/firmament/compat/jade/FirmamentJadePlugin.kt
@@ -1,15 +1,12 @@
 package moe.nea.firmament.compat.jade
 
-import snownee.jade.addon.harvest.HarvestToolProvider
 import snownee.jade.api.IWailaClientRegistration
 import snownee.jade.api.IWailaCommonRegistration
 import snownee.jade.api.IWailaPlugin
 import snownee.jade.api.WailaPlugin
 import net.minecraft.block.Block
-import net.minecraft.block.Blocks
 import moe.nea.firmament.Firmament
 
-// TODO: make this display breaking power, override names of mineable blocks, and override breaking indicator to use mining fatigue system
 @WailaPlugin
 class FirmamentJadePlugin : IWailaPlugin {
 	override fun register(registration: IWailaCommonRegistration) {
