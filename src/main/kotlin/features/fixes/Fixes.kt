4c3933a787b017dd115e0c908f2b56da22a85bf1
Linnea Gräf <nea@nea.moe>
2025-03-17 21:38:15 +0100

    feat: No Hurt Cam

--- src/main/kotlin/features/fixes/Fixes.kt
+++ src/main/kotlin/features/fixes/Fixes.kt
@@ -23,6 +23,7 @@ object Fixes : FirmamentFeature {
 		val autoSprintHud by position("auto-sprint-hud", 80, 10) { Point(0.0, 1.0) }
 		val peekChat by keyBindingWithDefaultUnbound("peek-chat")
 		val hidePotionEffects by toggle("hide-mob-effects") { false }
+		val noHurtCam by toggle("disable-hurt-cam") { false }
 	}
 
 	override val config: ManagedConfig
