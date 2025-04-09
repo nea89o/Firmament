72044baeff7da702a66b99afa084f3fe9ab7bbc2
Linnea Gräf <nea@nea.moe>
2025-04-09 16:41:58 +0200

    fix: Fix (some) parts of custom block rendering.

--- src/texturePacks/java/moe/nea/firmament/mixins/custommodels/ReplaceFallbackBlockModel.java
+++ src/texturePacks/java/moe/nea/firmament/mixins/custommodels/ReplaceFallbackBlockModel.java
@@ -3,7 +3,7 @@ package moe.nea.firmament.mixins.custommodels;
 import moe.nea.firmament.features.texturepack.CustomBlockTextures;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.render.block.BlockModels;
-import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.BlockStateModel;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ReplaceFallbackBlockModel {
     // TODO: add check to BlockDustParticle
     @Inject(method = "getModel", at = @At("HEAD"), cancellable = true)
-    private void getModel(BlockState state, CallbackInfoReturnable<BakedModel> cir) {
+    private void getModel(BlockState state, CallbackInfoReturnable<BlockStateModel> cir) {
         var replacement = CustomBlockTextures.getReplacementModel(state, null);
         if (replacement != null)
             cir.setReturnValue(replacement);
