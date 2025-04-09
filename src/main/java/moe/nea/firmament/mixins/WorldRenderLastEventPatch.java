0abfc0068ccf6c1efae91c97ce09a8809eb5da51
Linnea Gräf <nea@nea.moe>
2025-04-09 13:26:26 +0200

    fix: Build on 1.21.5 with some less compat layers enabled

--- src/main/java/moe/nea/firmament/mixins/WorldRenderLastEventPatch.java
+++ src/main/java/moe/nea/firmament/mixins/WorldRenderLastEventPatch.java
@@ -2,11 +2,9 @@
 
 package moe.nea.firmament.mixins;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import moe.nea.firmament.events.WorldRenderLastEvent;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.Handle;
-import net.minecraft.client.util.ObjectAllocator;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.profiler.Profiler;
 import org.joml.Matrix4f;
@@ -31,12 +29,12 @@ public abstract class WorldRenderLastEventPatch {
 	protected abstract void checkEmpty(MatrixStack matrices);
 
 	@Inject(method = "method_62214", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;pop()V", shift = At.Shift.AFTER))
-	public void onWorldRenderLast(Fog fog, RenderTickCounter tickCounter, Camera camera, Profiler profiler, Matrix4f matrix4f, Matrix4f matrix4f2, Handle handle, Handle handle2, Handle handle3, Handle handle4, boolean bl, Frustum frustum, Handle handle5, CallbackInfo ci) {
+	public void onWorldRenderLast(Fog fog, RenderTickCounter renderTickCounter, Camera camera, Profiler profiler, Matrix4f matrix4f, Matrix4f matrix4f2, Handle handle, Handle handle2, boolean bl, Frustum frustum, Handle handle3, Handle handle4, CallbackInfo ci) {
 		var imm = this.bufferBuilders.getEntityVertexConsumers();
 		var stack = new MatrixStack();
 		// TODO: pre-cancel this event if F1 is active
 		var event = new WorldRenderLastEvent(
-			stack, tickCounter,
+			stack, renderTickCounter,
 			camera,
 			imm
 		);
