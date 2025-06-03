9ad691bc1ba5f50d89a9c99cbf950fe2390a5a44
Linnea Gräf <nea@nea.moe>
2025-06-04 00:19:51 +0200

    feat: Prettier price formatting
    Co-authored-by: jani270 <69345714+jani270@users.noreply.github.com>

--- src/main/kotlin/features/inventory/PriceData.kt
+++ src/main/kotlin/features/inventory/PriceData.kt
@@ -1,5 +1,3 @@
-
-
 package moe.nea.firmament.features.inventory
 
 import net.minecraft.text.Text
@@ -8,44 +6,68 @@ import moe.nea.firmament.events.ItemTooltipEvent
 import moe.nea.firmament.features.FirmamentFeature
 import moe.nea.firmament.gui.config.ManagedConfig
 import moe.nea.firmament.repo.HypixelStaticData
-import moe.nea.firmament.util.FirmFormatters
+import moe.nea.firmament.util.FirmFormatters.formatCommas
+import moe.nea.firmament.util.bold
+import moe.nea.firmament.util.gold
 import moe.nea.firmament.util.skyBlockId
+import moe.nea.firmament.util.tr
+import moe.nea.firmament.util.yellow
 
 object PriceData : FirmamentFeature {
-    override val identifier: String
-        get() = "price-data"
+	override val identifier: String
+		get() = "price-data"
 
-    object TConfig : ManagedConfig(identifier, Category.INVENTORY) {
-        val tooltipEnabled by toggle("enable-always") { true }
-        val enableKeybinding by keyBindingWithDefaultUnbound("enable-keybind")
-    }
+	object TConfig : ManagedConfig(identifier, Category.INVENTORY) {
+		val tooltipEnabled by toggle("enable-always") { true }
+		val enableKeybinding by keyBindingWithDefaultUnbound("enable-keybind")
+	}
 
-    override val config get() = TConfig
+	override val config get() = TConfig
 
-    @Subscribe
-    fun onItemTooltip(it: ItemTooltipEvent) {
-        if (!TConfig.tooltipEnabled && !TConfig.enableKeybinding.isPressed()) {
-            return
-        }
-        val sbId = it.stack.skyBlockId
-        val bazaarData = HypixelStaticData.bazaarData[sbId]
-        val lowestBin = HypixelStaticData.lowestBin[sbId]
-        if (bazaarData != null) {
-            it.lines.add(Text.literal(""))
-            it.lines.add(
-                Text.stringifiedTranslatable("firmament.tooltip.bazaar.sell-order",
-                                             FirmFormatters.formatCommas(bazaarData.quickStatus.sellPrice, 1))
-            )
-            it.lines.add(
-                Text.stringifiedTranslatable("firmament.tooltip.bazaar.buy-order",
-                                             FirmFormatters.formatCommas(bazaarData.quickStatus.buyPrice, 1))
-            )
-        } else if (lowestBin != null) {
-            it.lines.add(Text.literal(""))
-            it.lines.add(
-                Text.stringifiedTranslatable("firmament.tooltip.ah.lowestbin",
-                                             FirmFormatters.formatCommas(lowestBin, 1))
-            )
-        }
-    }
+	fun formatPrice(label: Text, price: Double): Text {
+		return Text.literal("")
+			.yellow()
+			.bold()
+			.append(label)
+			.append(": ")
+			.append(
+				Text.literal(formatCommas(price, fractionalDigits = 1))
+					.append(if(price != 1.0) " coins" else " coin")
+					.gold()
+					.bold()
+			)
+	}
+
+	@Subscribe
+	fun onItemTooltip(it: ItemTooltipEvent) {
+		if (!TConfig.tooltipEnabled && !TConfig.enableKeybinding.isPressed()) {
+			return
+		}
+		val sbId = it.stack.skyBlockId
+		val bazaarData = HypixelStaticData.bazaarData[sbId]
+		val lowestBin = HypixelStaticData.lowestBin[sbId]
+		if (bazaarData != null) {
+			it.lines.add(Text.literal(""))
+			it.lines.add(
+				formatPrice(
+					tr("firmament.tooltip.bazaar.sell-order", "Bazaar Sell Order"),
+					bazaarData.quickStatus.sellPrice
+				)
+			)
+			it.lines.add(
+				formatPrice(
+					tr("firmament.tooltip.bazaar.buy-order", "Bazaar Buy Order"),
+					bazaarData.quickStatus.buyPrice
+				)
+			)
+		} else if (lowestBin != null) {
+			it.lines.add(Text.literal(""))
+			it.lines.add(
+				formatPrice(
+					tr("firmament.tooltip.ah.lowestbin", "Lowest BIN"),
+					lowestBin
+				)
+			)
+		}
+	}
 }
