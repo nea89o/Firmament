e9a4ce5575e48ced171bac17181e5cdd18722c5a
Linnea Gräf <nea@nea.moe>
2025-07-01 14:12:12 +0200

    fix: copy chat not being registered

--- src/main/kotlin/features/chat/CopyChat.kt
+++ src/main/kotlin/features/chat/CopyChat.kt
@@ -1,6 +1,8 @@
 package moe.nea.firmament.features.chat
 
 import net.minecraft.text.OrderedText
+import moe.nea.firmament.annotations.Subscribe
+import moe.nea.firmament.events.ClientStartedEvent
 import moe.nea.firmament.features.FirmamentFeature
 import moe.nea.firmament.gui.config.ManagedConfig
 import moe.nea.firmament.util.reconstitute
@@ -12,6 +14,10 @@ object CopyChat : FirmamentFeature {
 
 	object TConfig : ManagedConfig(identifier, Category.CHAT) {
 		val copyChat by toggle("copy-chat") { false }
+	}
+
+	@Subscribe
+	fun onInit(event: ClientStartedEvent) {
 	}
 
 	override val config: ManagedConfig?
