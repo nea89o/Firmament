b09648d71258f8d4012c9b50d679f7c3db9201b2
Linnea Gräf <nea@nea.moe>
2025-07-06 23:34:56 +0200

    fix: incorrect macro wheel for two items because of angle lerp near π / 2 defaulting the wrong way

--- src/main/kotlin/util/render/LerpUtils.kt
+++ src/main/kotlin/util/render/LerpUtils.kt
@@ -1,11 +1,15 @@
 package moe.nea.firmament.util.render
 
 import me.shedaniel.math.Color
+import kotlin.math.absoluteValue
 
 val π = Math.PI
 val τ = Math.PI * 2
-fun  lerpAngle(a: Float, b: Float, progress: Float): Float {
+fun lerpAngle(a: Float, b: Float, progress: Float): Float {
 	// TODO: there is at least 10 mods to many in here lol
+	if (((b - a).absoluteValue - π).absoluteValue < 0.0001) {
+		return lerp(a, b, progress)
+	}
 	val shortestAngle = ((((b.mod(τ) - a.mod(τ)).mod(τ)) + τ + π).mod(τ)) - π
 	return ((a + (shortestAngle) * progress).mod(τ)).toFloat()
 }
