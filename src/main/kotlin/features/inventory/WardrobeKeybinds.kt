fd10e7fe58d091e6aa84a61b25146e511246fd2d
Linnea Gräf <nea@nea.moe>
2025-12-06 21:35:10 +0100

    fix: wardrobe keybinds
    (cherry picked from commit 65609163d39da7c3a1787f127045d772c77b0f1c)

--- src/main/kotlin/features/inventory/WardrobeKeybinds.kt
+++ src/main/kotlin/features/inventory/WardrobeKeybinds.kt
@@ -30,7 +30,7 @@ object WardrobeKeybinds {
 	fun switchSlot(event: HandledScreenKeyPressedEvent) {
 		if (MC.player == null || MC.world == null || MC.interactionManager == null) return
 
-		val regex = Regex("Wardrobe \\([12]/2\\)")
+		val regex = Regex("Wardrobe \\([0-9]+/[0-9]+\\)")
 		if (!regex.matches(event.screen.title.string)) return
 		if (!TConfig.wardrobeKeybinds) return
 
