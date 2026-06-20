1519bc2e954d1f27235271567a218e87aaabc8e7
Wapic <17051068+Wapic@users.noreply.github.com>
2026-06-20 16:27:12 +0200

    remove testing stuff

--- src/main/java/moe/nea/firmament/mixins/customgui/PatchHandledScreen.java
+++ src/main/java/moe/nea/firmament/mixins/customgui/PatchHandledScreen.java
@@ -99,10 +99,6 @@ public class PatchHandledScreen<T extends AbstractContainerMenu> extends Screen 
 	@Inject(method = "init", at = @At("TAIL"))
 	private void onInit(CallbackInfo ci) {
 		if (override != null) {
-			int totalWidth = override.getBounds().getFirst().width;
-			int totalHeight = override.getBounds().getFirst().height - 3 + override.getBounds().get(1).height;
-			this.leftPos = (this.width - totalWidth) / 2;
-			this.topPos = (this.height - totalHeight) / 2;
 			override.onInit();
 		}
 	}
